a350992df3d76cca3fa641afd1909d08cc0071f3
Bredosen <Bredosen@dr.com>
2024-02-19 07:39:18 +0100

    Add project files.

---
+++ Source/WhiskersOfTheWild/WhiskersOfTheWild.cs
@@ -0,0 +1,31 @@
+﻿namespace Whiskers_Of_The_Wild.Source.WhiskersOfTheWild
+{
+    public partial class WhiskersOfTheWild
+    {
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Terminate()
+        {
+
+        }
+
+    }
+
+    public partial class WhiskersOfTheWild
+    {
+        private static readonly WhiskersOfTheWild _instance = new();
+        private WhiskersOfTheWild() { }
+
+        public static void Main()
+        {
+            GetInstance().Initialize();
+            GetInstance().Terminate();
+        }
+
+        public static WhiskersOfTheWild GetInstance() => _instance;
+    }
+}
